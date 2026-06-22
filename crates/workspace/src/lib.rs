464c1bacc3063b520c5f88cf2edb77152633b06b
SpuerMomonga <1638829841@qq.com>
2026-06-22 17:57:42 +0800

    feat: add kw_workspace crate and integrate workspace functionality

--- crates/workspace/src/lib.rs
+++ crates/workspace/src/lib.rs
@@ -1,1 +1,39 @@
+use gpui::{Context, IntoElement, Render, Window, div, prelude::*, px, rgb};
 
+pub struct Workspace {}
+
+impl Workspace {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Render for Workspace {
+    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
+        div()
+            .flex()
+            .flex_col()
+            .gap_3()
+            .bg(rgb(0x505050))
+            .size(px(500.0))
+            .justify_center()
+            .items_center()
+            .shadow_lg()
+            .border_1()
+            .border_color(rgb(0x0000ff))
+            .text_xl()
+            .text_color(rgb(0xffffff))
+            .child(format!("Hello, {}!", "world"))
+            .child(
+                div()
+                    .flex()
+                    .gap_2()
+                    .child(div().size_8().bg(gpui::red()))
+                    .child(div().size_8().bg(gpui::green()))
+                    .child(div().size_8().bg(gpui::blue()))
+                    .child(div().size_8().bg(gpui::yellow()))
+                    .child(div().size_8().bg(gpui::black()))
+                    .child(div().size_8().bg(gpui::white())),
+            )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
-use gpui::Application;
+use gpui::{
+    App, AppContext, Application, TitlebarOptions, WindowBounds, WindowOptions, bounds, point, px,
+    size,
+};
 use gpui_platform;
+use kw_workspace::Workspace;
 
 fn main() {
     kw_tracing::init();
 
     let app = Application::with_platform(gpui_platform::current_platform(false));
 
-    app.run(move |_cx| {
+    app.run(move |cx| {
         kw_i18n::init("en");
+
+        initialize_workspace(cx);
     });
+}
+
+fn initialize_workspace(cx: &mut App) {
+    cx.open_window(
+        WindowOptions {
+            titlebar: Some(TitlebarOptions {
+                title: Some("GPUI Typography".into()),
+                ..Default::default()
+            }),
+            window_bounds: Some(WindowBounds::Windowed(bounds(
+                point(px(0.0), px(0.0)),
+                size(px(920.), px(720.)),
+            ))),
+            ..Default::default()
+        },
+        |_window, cx| cx.new(|_cx| Workspace::new()),
+    ).unwrap();
 }
